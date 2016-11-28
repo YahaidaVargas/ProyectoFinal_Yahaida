504614b2c7176041c2f43e53ee5f97d433abae20
YahaidaVargas <lafenix2003@gmail.com>
2016-11-28 16:24:55 -0400

    Cobros y cobro detalle

--- BLL/CobroDetalle.cs
+++ BLL/CobroDetalle.cs
@@ -9,30 +9,79 @@ namespace BLL
 {
     public class CobroDetalle : ClaseMaestra
     {
+        public int IdCobrosDetalle{ get; set; }
+        public int IdCobros { get; set; }
+        public string FechaMens { get; set; }
+        public int Monto { get; set; }
+        public string Descripcion { get; set; }
+        public int DescuentoCobrosDetalle { get; set; }
+        public string FormaPago { get; set; }
+        public int Total { get; set; }
 
+        public CobroDetalle()
+        {
+            IdCobrosDetalle = 0;
+            IdCobros = 0;
+            FechaMens = "";
+            Monto = 0;
+            Descripcion = "";
+            DescuentoCobrosDetalle = 0;
+            FormaPago = "";
+            Total = 0;
+        }
         public override bool Insertar()
         {
-            throw new NotImplementedException();
+            ConexionDb conexion = new ConexionDb();
+            string consulta = string.Format("insert into CobrosDetalle(IdCobros,FechaMens,Monto,Descripcion,DescuentoCobrosDetalle,FormaDePago,Total) values({0},'{1}',{2},'{3}',{4},'{5}',{6}) SELECT @@IDENTITY", IdCobros, FechaMens, Monto,Descripcion, DescuentoCobrosDetalle, FormaPago, Total);
+
+            IdCobrosDetalle = Convert.ToInt32(conexion.ObtenerValorDb(consulta).ToString());
+            return IdCobrosDetalle > 0;
         }
 
         public override bool Editar()
         {
-            throw new NotImplementedException();
+            ConexionDb conexion = new ConexionDb();
+
+            string sql = string.Format("UPDATE CobrosDetalle SET IdCobros = {0}, FechaMens = '{1}', Monto= {2}, Descripcion = '{3}', DescuentoCobrosDetalle = {4}, FormaPago = '{5}', Total = {6} WHERE IdCobrosDetalle = {7}", IdCobros, FechaMens ,Monto ,Descripcion ,DescuentoCobrosDetalle ,FormaPago , Total, IdCobrosDetalle);
+            return conexion.EjecutarDB(sql);
         }
 
         public override bool Eliminar()
         {
-            throw new NotImplementedException();
+            ConexionDb conexion = new ConexionDb();
+
+            string sql = string.Format("DELETE FROM CobrosDetalle WHERE IdCobrosDetalle = {0}", IdCobrosDetalle);
+            return conexion.EjecutarDB(sql);
         }
 
         public override bool Buscar(int IdBuscado)
         {
-            throw new NotImplementedException();
+            ConexionDb conexion = new ConexionDb();
+
+            string sql = string.Format("SELECT * FROM Cobros WHERE IdCobros = {0}", IdBuscado);
+
+            DataTable dt = conexion.BuscarDb(sql);
+
+            if (dt.Rows.Count > 0)
+            {
+                IdCobrosDetalle = Convert.ToInt32(dt.Rows[0]["IdCobrosDetalle"]);
+                IdCobros = Convert.ToInt32(dt.Rows[0]["IdCobros "]);               
+                FechaMens = dt.Rows[0]["FechaMens"].ToString();
+                Monto = Convert.ToInt32(dt.Rows[0]["Monto"]);
+                Descripcion = dt.Rows[0]["Descripcion"].ToString();
+                DescuentoCobrosDetalle = Convert.ToInt32(dt.Rows[0]["DescuentoCobrosDetalle"]);
+                FormaPago = dt.Rows[0][" FormaPago"].ToString();
+                Total = Convert.ToInt32(dt.Rows[0]["Total"]);
+
+            }
+
+            return dt.Rows.Count > 0;
         }
 
-        public override DataTable Listado(string Campos, string Condicion, string Orden)
+        public override DataTable Listado(string Campos = "*", string Condicion = "1=1", string Orden = "ASC")
         {
-            throw new NotImplementedException();
+            ConexionDb conexion = new ConexionDb();
+            return conexion.BuscarDb("Select " + Campos + " from CobrosDetalle where " + Condicion + " order by " + Orden);
         }
 
 
--- BLL/Cobros.cs
+++ BLL/Cobros.cs
@@ -9,30 +9,80 @@ namespace BLL
 {
     public class Cobros : ClaseMaestra
     {
-        public override bool Buscar(int IdBuscado)
+        public int IdCobros { get; set; }
+        public int  IdInscripcion { get; set; }
+        public string Fecha { get; set; }
+        public int Monto { get; set; }
+        public int Descuento { get; set; }
+        public string FormaDePago { get; set; }
+        public int Total { get; set; }
+
+
+        public Cobros(){
+            IdCobros = 0;
+            IdInscripcion = 0;
+            Fecha = "";
+            Monto = 0;
+            Descuento = 0;
+            FormaDePago = "";
+            Total = 0;
+            }
+
+        public override bool Insertar()
         {
-            throw new NotImplementedException();
+            ConexionDb conexion = new ConexionDb();
+            string consulta = string.Format("insert into Cobros(IdInscripcion,Fecha,Monto,Descuento,FormaDePago,Total) values({0},'{1}',{2},{3},'{4}',{5}) SELECT @@IDENTITY", IdInscripcion, Fecha, Monto, Descuento, FormaDePago, Total);
+
+            IdCobros = Convert.ToInt32(conexion.ObtenerValorDb(consulta).ToString());
+            return IdCobros > 0;
         }
+
 
         public override bool Editar()
         {
-            throw new NotImplementedException();
+            ConexionDb conexion = new ConexionDb();
+
+            string sql = string.Format("UPDATE Cobros SET IdInscripcion = {0}, Fecha = '{1}', Monto= {2}, Descuento = {3}, FormaDePago = '{4}', Total = {5} WHERE IdCobros = {6}", IdInscripcion, Fecha, Monto, Descuento,FormaDePago,Total, IdCobros);
+            return conexion.EjecutarDB(sql);
         }
 
         public override bool Eliminar()
         {
-            throw new NotImplementedException();
+            ConexionDb conexion = new ConexionDb();
+
+            string sql = string.Format("DELETE FROM Cobros WHERE IdCobros = {0}", IdCobros);
+            return conexion.EjecutarDB(sql);
         }
 
-        public override bool Insertar()
+        public override bool Buscar(int IdBuscado)
         {
-            throw new NotImplementedException();
+            ConexionDb conexion = new ConexionDb();
+
+            string sql = string.Format("SELECT * FROM Cobros WHERE IdCobros = {0}", IdBuscado);
+
+            DataTable dt = conexion.BuscarDb(sql);
+
+            if (dt.Rows.Count > 0)
+            {
+                IdCobros = Convert.ToInt32(dt.Rows[0]["IdCobros "]);
+                IdInscripcion = Convert.ToInt32(dt.Rows[0]["IdInscripcion"]);
+                Fecha = dt.Rows[0]["Fecha"].ToString();
+                Monto = Convert.ToInt32(dt.Rows[0]["Monto"]);
+                Descuento = Convert.ToInt32(dt.Rows[0]["Descuento"]);
+                FormaDePago = dt.Rows[0]["FormaDePago"].ToString();
+                Total = Convert.ToInt32(dt.Rows[0]["Total"]);
+             
+            }
+
+            return dt.Rows.Count > 0;
         }
 
-        public override DataTable Listado(string Campos, string Condicion, string Orden)
+        public override DataTable Listado(string Campos = "*", string Condicion = "1=1", string Orden = "ASC")
         {
-            throw new NotImplementedException();
+            ConexionDb conexion = new ConexionDb();
+            return conexion.BuscarDb("Select " + Campos + " from Cobros where " + Condicion + " order by " + Orden);
         }
+
 
         ///REPORTES
         ///
